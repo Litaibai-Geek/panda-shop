ee01d524d56ef42a98bb79f7531c286a59bf4f85
Litaibai_Geek <251253728@qq.com>
2021-09-27 23:15:36 +0800

    整合Swagger

--- panda-shop-service-api/panda-shop-service-api-member/src/main/java/com/panda/shop/member/service/MemberService.java
+++ panda-shop-service-api/panda-shop-service-api-member/src/main/java/com/panda/shop/member/service/MemberService.java
@@ -1,14 +1,18 @@
 package com.panda.shop.member.service;
 
 import com.panda.shop.entity.AppEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+@Api(tags = "会员服务实现接口")
 public interface MemberService {
     /**
      * 会员调用微信
      * @return
      */
+    @ApiOperation(value = "会员服务调用微信服务")
     @GetMapping("/memberToWeiXin")
     public AppEntity memberToWeiXin();
 }
--- panda-shop-service-api/panda-shop-service-api-weixin/src/main/java/com/panda/shop/weixin/service/WeiXinService.java
+++ panda-shop-service-api/panda-shop-service-api-weixin/src/main/java/com/panda/shop/weixin/service/WeiXinService.java
@@ -1,10 +1,14 @@
 package com.panda.shop.weixin.service;
 
 import com.panda.shop.entity.AppEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Api(tags = "微信服务接口")
 public interface WeiXinService {
 
+    @ApiOperation(value = "微信应用服务接口")
     @GetMapping("/getApp")
     public AppEntity getApp();
 }
