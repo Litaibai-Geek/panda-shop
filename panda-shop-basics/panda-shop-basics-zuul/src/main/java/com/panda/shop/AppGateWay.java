2537ba31ad0e850200ecddd0513ecec76fa82405
Litaibai_Geek <251253728@qq.com>
2021-10-04 13:39:00 +0800

    阿波罗集成Swagger

--- panda-shop-basics/panda-shop-basics-zuul/src/main/java/com/panda/shop/AppGateWay.java
+++ panda-shop-basics/panda-shop-basics-zuul/src/main/java/com/panda/shop/AppGateWay.java
@@ -1,5 +1,10 @@
 package com.panda.shop;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,7 +27,11 @@ import java.util.List;
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableSwagger2Doc
+@EnableApolloConfig
 public class AppGateWay {
+
+    @ApolloConfig
+    private Config config;
 
     public static void main(String[] args) {
         SpringApplication.run(AppGateWay.class, args);
@@ -32,13 +41,45 @@ public class AppGateWay {
     @Component
     @Primary
     class DocumentationConfig implements SwaggerResourcesProvider {
+        // 访问swagger-ui页面每次都会访问get方法
+        @SuppressWarnings("rawtypes")
         @Override
         public List<SwaggerResource> get() {
             List resources = new ArrayList<>();
-            // 网关使用别名获取远程服务的SwaggerApi
-            resources.add(swaggerResource("app-panda-shop-member", "/app-panda-shop-member/v2/api-docs", "2.0"));
-            resources.add(swaggerResource("app-panda-shop-weixin", "/app-panda-shop-weixin/v2/api-docs", "2.0"));
+            // 从阿波罗中获取配置信息
+            return resources();
+        }
+
+        /**
+         * 从阿波罗服务器中获取resources
+         *
+         * @return
+         */
+        private List<SwaggerResource> resources() {
+
+            List resources = new ArrayList<>();
+            // app-itmayiedu-order
+            // 网关使用服务别名获取远程服务的SwaggerApi
+            String swaggerDocJson = swaggerDocument();
+            JSONArray jsonArray = JSONArray.parseArray(swaggerDocJson);
+            for (Object object : jsonArray) {
+                JSONObject jsonObject = (JSONObject) object;
+                String name = jsonObject.getString("name");
+                String location = jsonObject.getString("location");
+                String version = jsonObject.getString("version");
+                resources.add(swaggerResource(name, location, version));
+            }
             return resources;
+        }
+
+        /**
+         * 获取swaggerDocument配置
+         *
+         * @return
+         */
+        private String swaggerDocument() {
+            String property = config.getProperty("panda.shop.zuul.swaggerDocument", "");
+            return property;
         }
 
         private SwaggerResource swaggerResource(String name, String location, String version) {
